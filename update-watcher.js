3d3017ec11f8b0a2938e450948dc55ec05bd93e8
Alex Harrison <alexjharrison@gmail.com>
2018-07-28 17:50:01 -0400

    should? pull from github and restart app

---
+++ update-watcher.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var cmd = require("node-cmd");
+
+var PORT = 3569;
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(PORT,(err)=>{
+    if (err) throw err;
+    console.log("Program started on port "+PORT);
+})
+
+app.post("/update",(req,res)=>{
+    if(req.body.ref==="refs/heads/master")
+    {
+        var repo = req.repository.name;
+        console.log("master branch");
+        cmd.get(
+            `
+            cd ../${repo}
+            sudo forever stop server.js
+            git pull
+            sudo forever start server.js
+            `,
+            function(err,data,stderr){
+                if(err) throw err;
+                console.log(data);
+                console.log("repo updated and restarted");
+                res.send(req.body);
+            }
+        )
+    }
+    else {
+        console.log("not master branch");
+        res.send(req.body)
+    }
+})
